9e2f92cbc11d197f071daa288b4d23dd6570ad36
JB <jmb2883@gmail.com>
2018-04-04 21:55:25 -0400

    adding code to generate output to the page

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+var wins = 0;
+var losses = 0;
+var crystalNumber = 0;
+var crystalTotal = 0;
+
+//function for getting the button value and printing it
+
+$(".row4").on("click", ".crystal", function () {
+  if(crystalTotal < crystalNumber) {
+    crystalTotal += parseInt($(this).attr("value"));
+    $("#crystalTotal").text(crystalTotal);
+    } else if (crystalTotal === crystalNumber) {
+      wins++;
+      $("#wins").text(wins);
+      reset();
+    }else {
+      losses++;
+      $("#losses").text(losses);
+      reset();
+    }
+});
+
+//function for generating a random crystal number
+
+function randomCrystal(min, max) {
+  crystalNumber = Math.floor(Math.random() * (max - min)) + min;
+  $("#randomNumber").text(crystalNumber)
+  return crystalNumber
+}
+
+function reset () {
+  randomCrystal(19,130);
+  crystalTotal = 0;
+  $("#crystalTotal").text(crystalTotal);
+}
+
+randomCrystal(19,130);
+
+});
