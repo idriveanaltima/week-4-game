1ad10f830a0f42a63881ee9c4f9773588a44c0ac
JB <jmb2883@gmail.com>
2018-04-06 17:19:45 -0400

    adding images and upddating UI

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,11 @@ var losses = 0;
 var crystalNumber = 0;
 var crystalTotal = 0;
 
+$("#crystal1").html("<img style='height: 100px; width: 100px; border-radius: 50%' src='assets/images/crystal1.gif'/>")
+$("#crystal2").html("<img style='height: 100px; width: 100px; border-radius: 50%' src='assets/images/crystal2.gif'/>")
+$("#crystal3").html("<img style='height: 100px; width: 100px; border-radius: 50%' src='assets/images/crystal3.gif'/>")
+$("#crystal4").html("<img style='height: 100px; width: 100px; border-radius: 50%' src='assets/images/crystal4.gif'/>")
+
 //function for getting the button value and printing it
 
 $(".row4").on("click", ".crystal", function () {
@@ -31,11 +36,11 @@ function randomCrystal(min, max) {
 }
 
 function reset () {
-  randomCrystal(19,130);
+  randomCrystal(19,120);
   crystalTotal = 0;
   $("#crystalTotal").text(crystalTotal);
 }
 
-randomCrystal(19,130);
+randomCrystal(19,120);
 
 });
