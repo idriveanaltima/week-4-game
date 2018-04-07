fcbd47bbe9d17cd3f0e331f6478ac577b304b1db
JB <jmb2883@gmail.com>
2018-04-07 06:43:34 -0400

    updating UI

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,19 @@
-$(document).ready(function() {
+//Declaring global variables for the game
 
 var wins = 0;
 var losses = 0;
 var crystalNumber = 0;
 var crystalTotal = 0;
+//make sure the dom is loaded before traversing it
 
-$("#crystal1").html("<img style='height: 100px; width: 100px; border-radius: 50%' src='assets/images/crystal1.gif'/>")
-$("#crystal2").html("<img style='height: 100px; width: 100px; border-radius: 50%' src='assets/images/crystal2.gif'/>")
-$("#crystal3").html("<img style='height: 100px; width: 100px; border-radius: 50%' src='assets/images/crystal3.gif'/>")
-$("#crystal4").html("<img style='height: 100px; width: 100px; border-radius: 50%' src='assets/images/crystal4.gif'/>")
+$(document).ready(function() {
+
+// addimg the images to the html
+
+$("#crystal1").html("<img style='height: 75px; width: 75px; border-radius: 50%' src='assets/images/crystal1.gif'/>")
+$("#crystal2").html("<img style='height: 75px; width: 75px; border-radius: 50%' src='assets/images/crystal2.gif'/>")
+$("#crystal3").html("<img style='height: 75px; width: 75px; border-radius: 50%' src='assets/images/crystal3.gif'/>")
+$("#crystal4").html("<img style='height: 75px; width: 75px; border-radius: 50%' src='assets/images/crystal4.gif'/>")
 
 //function for getting the button value and printing it
 
@@ -29,18 +34,20 @@ $(".row4").on("click", ".crystal", function () {
 
 //function for generating a random crystal number
 
-function randomCrystal(min, max) {
+function randomNumber(min, max) {
   crystalNumber = Math.floor(Math.random() * (max - min)) + min;
-  $("#randomNumber").text(crystalNumber)
-  return crystalNumber
+  $("#randomNumber").text(crystalNumber) 
 }
 
+//function for resetting the game
 function reset () {
-  randomCrystal(19,120);
+  randomNumber(19,120);
   crystalTotal = 0;
   $("#crystalTotal").text(crystalTotal);
 }
 
-randomCrystal(19,120);
+//calling the functions on page load
+
+randomNumber(19,120);
 
 });
